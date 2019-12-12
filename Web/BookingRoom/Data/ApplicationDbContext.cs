1192368fabbba2b6fc22e4435052ca4dc9a52eb2
Андрій Лесів <aner98lam@ukr.net>
2019-12-12 20:31:05 +0200

    Project was created.

---
+++ Web/BookingRoom/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BookingRoom.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingRoom.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
